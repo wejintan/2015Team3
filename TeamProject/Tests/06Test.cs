7094d115b6ad07456cd7be9f586b749b1422371f
JiaFong <j530621@gmail.com>
2015-12-13 20:59:56 +0800

    TEST06

---
+++ TeamProject/Tests/06Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProject.Tests
+{
+    [TestFixture]
+    public class _06Test
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That("Team 3", Is.EqualTo("Team 3"));
+        }
+    }
+}
