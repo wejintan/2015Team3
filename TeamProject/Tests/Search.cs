2b2d79393ac0868ee1e5278f485d3c48f57c41b9
wejintan <wejintan@yahoo.com.tw>
2015-12-13 21:30:52 +0800

    Test

---
+++ TeamProject/Tests/Search.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProject.Tests
+{
+    [TestFixture]
+    class Search
+    {
+        [Test]
+        public void Search1()
+        {
+            Assert.That("John", Is.EqualTo("John"));
+        }
+    }
+}
